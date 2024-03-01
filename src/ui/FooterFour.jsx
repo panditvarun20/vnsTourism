a4ed716064c05dfd6b348debc5ad142191e84942
panditvarun20 <varpa20@gmail.com>
2024-03-01 18:05:07 +0530

    navbarsolved

--- src/ui/FooterFour.jsx
+++ src/ui/FooterFour.jsx
@@ -2,36 +2,36 @@ import React from 'react'
 
 const FooterFour = () => {
   return (
-    <div class="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
+    <div className="col-lg-4 col-md-12 pt-0 pt-lg-5 mb-5">
     <div
-      class="section-title section-title-sm position-relative pb-3 mb-4"
+      className="section-title section-title-sm position-relative pb-3 mb-4"
     >
-      <h3 class="text-light mb-0">Popular Links</h3>
+      <h3 className="text-light mb-0">Popular Links</h3>
     </div>
     <div
-      class="link-animated d-flex flex-column justify-content-start"
+      className="link-animated d-flex flex-column justify-content-start"
     >
-      <a class="text-light mb-2" href="index.html"
-        ><i class="bi bi-arrow-right text-primary me-2"></i>Home</a
+      <a className="text-light mb-2" href="index.html"
+        ><i className="bi bi-arrow-right text-primary me-2"></i>Home</a
       >
-      <a class="text-light mb-2" href="about_us.html"
-        ><i class="bi bi-arrow-right text-primary me-2"></i>About
+      <a className="text-light mb-2" href="about_us.html"
+        ><i className="bi bi-arrow-right text-primary me-2"></i>About
         Us</a
       >
-      <a class="text-light mb-2" href="tourist-places.html"
-        ><i class="bi bi-arrow-right text-primary me-2"></i
+      <a className="text-light mb-2" href="tourist-places.html"
+        ><i className="bi bi-arrow-right text-primary me-2"></i
         >Tourism</a
       >
-      <a class="text-light mb-2" href="history.html"
-        ><i class="bi bi-arrow-right text-primary me-2"></i
+      <a className="text-light mb-2" href="history.html"
+        ><i className="bi bi-arrow-right text-primary me-2"></i
         >History</a
       >
-      <a class="text-light" href="photo_gallery.html"
-        ><i class="bi bi-arrow-right text-primary me-2"></i
+      <a className="text-light" href="photo_gallery.html"
+        ><i className="bi bi-arrow-right text-primary me-2"></i
         >Gallery</a
       >
-      <a class="text-light mb-2 mt-2" href="contact.html"
-        ><i class="bi bi-arrow-right text-primary me-2"></i>Contact
+      <a className="text-light mb-2 mt-2" href="contact.html"
+        ><i className="bi bi-arrow-right text-primary me-2"></i>Contact
         Us</a
       >
     </div>
--- src/ui/Navbar.jsx
+++ src/ui/Navbar.jsx
@@ -1,98 +1,65 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NavLink } from "react-router-dom";
+import logoImage from '../assets/img/vendor-2.jpg'
+
 const Navbar = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const username = "admin"; // Replace with your actual username
+      const password = "admin"; // Replace with your actual password
+
+      const response = await fetch(
+        "http://local.tourism//api/menu_items/main",
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Basic ${btoa(`${username}:${password}`)}`,
+            "Access-Control-Allow-Origin": "*",
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+
+      const jsonData = await response.json();
+      setData(jsonData);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
   return (
-    <div className="container-fluid position-relative p-0">
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-5 py-3 py-lg-0">
-        <NavLink to="index.html" className="navbar-brand p-0">
-          <h1 className="m-0">
-            <img src="india/Logos.png" className="w-75" alt="" srcset="" />
-          </h1>
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarCollapse"
-        >
-          <span className="fa fa-bars"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarCollapse">
-          <div className="navbar-nav ms-auto py-0">
-            <NavLink to="index.html" className="nav-item nav-link active">
-              Home
-            </NavLink>
-            <NavLink to="about_us.html" className="nav-item nav-link">
-              About Us
-            </NavLink>
-            <NavLink to="tourist-places.html" className="nav-item nav-link">
-              Tourist Places
-            </NavLink>
-            <div className="nav-item dropdown">
-              <NavLink
-                to="index.html"
-                className="nav-link dropdown-toggle"
-                data-bs-toggle="dropdown"
-              >
-                History
-              </NavLink>
-              <div className="dropdown-menu m-0">
-                <NavLink to="history.html" className="dropdown-item">
-                  History
-                </NavLink>
-                <NavLink to="excursions.html" className="dropdown-item">
-                  Excursions
-                </NavLink>
-              </div>
-            </div>
-            <div className="nav-item dropdown">
-              <NavLink
-                to="photo_gallery.html"
-                className="nav-link dropdown-toggle"
-                data-bs-toggle="dropdown"
-              >
-                Gallery
-              </NavLink>
-              <div className="dropdown-menu m-0">
-                <NavLink to="ganga_aarti.html" className="dropdown-item">
-                  Ganga Aasrti
-                </NavLink>
-                <NavLink to="aasi_ghat.html" className="dropdown-item">
-                  Assi Ghat
-                </NavLink>
-                <NavLink to="sarnath.html" className="dropdown-item">
-                  Sarnata Tempal
-                </NavLink>
-                <NavLink to="kashi_vishwanath.html" className="dropdown-item">
-                  Kashi Vishwanath Tempal
-                </NavLink>
-                <NavLink to="bhu.html" className="dropdown-item">
-                  Banaras Hindu University
-                </NavLink>
-              </div>
-            </div>
-            <NavLink to="contact.html" className="nav-item nav-link">
-              Contact Us
-            </NavLink>
-          </div>
-          <div className="text-center">
-            <butaton
-              type="button"
-              className="btn text-primary ms-3 text-center"
-              data-bs-toggle="modal"
-              data-bs-target="#searchModal"
-            >
-              <i className="fa fa-search d-flex"></i>
-            </butaton>
-            <NavLink
-              to="index.html"
-              className="btn btn-primary py-2 px-4 ms-3 rounded-3 text-center"
-            >
-              Go To Visit
-            </NavLink>
-          </div>
-        </div>
-      </nav>
+    <div >
+      <img src={logoImage} alt="" />
+      <ul className="nav">
+        {data.map((item) => (
+          <li key={item.key} className="nav-item">
+            <a href="#" className="nav-link">
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
